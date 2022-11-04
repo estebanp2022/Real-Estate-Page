4c49b1ad08136896e6d62af9b73bd5a1c7ecab53
estebanp2022 <91852753+estebanp2022@users.noreply.github.com>
2022-11-03 20:35:55 -0400

    file upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let storyBackground = document.querySelector(".story-pictures");
+
+async function getPhotos() {
+  let response = await fetch(
+    "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=interiors&count=10"
+  );
+  let photos = await response.json();
+  return photos;
+}
+
+getPhotos().then((photos) => {
+  console.log(photos);
+  storyBackground.innerHTML = `
+        <img class="img-1" src="${photos[0].urls.small}">
+        <img class="img-2" src="${photos[1].urls.small}">
+    `;
+  storyBackground.style.backgroundImage = `url(${photos[3].urls.regular})`;
+});
+
+// getPhotos().then((photos) => {
+//   console.log(photos);
+//   img1.style.backgroundImage = `url(
+//     ${photos[0].urls.small}
+//     )`;
+//   img2.style.backgroundImage = `url(
+//     ${photos[1].urls.small}
+//     )`;
+//   images.style.backgroundImage = `url(
+//     ${photos[3].urls.regular}
+//     )`;
+// });
